b6ea9c9257a8556fa4210083780a7798b9a038d2
Unknown <joelanto386@gmail.com>
2025-03-31 16:04:40 +0530

    updated login card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Card(){
-  function onButtonClick() {
-    alert("Logged in")
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  function onButtonClick(event) {
+    event.preventDefault()
+    if(email == '' || password == ''){
+      alert("User doesn't exist")
+    } else {
+      alert("Welcome")
+    }
   }
   return (
     <>
       <div className='login-card'>
+        <h2> React Study Login</h2>
         <input
           type="text"
           name="email"
+          value={email}
           placeholder="Enter Email Address"
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           name="password"
+          value={password}
           placeholder="Enter Password"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button className='login-btn' onClick={onButtonClick}> Login Now </button>
       </div>
