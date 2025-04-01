e0109583cbf1f5692742e92de91bcc13ac16db76
Unknown <joelanto386@gmail.com>
2025-04-01 10:41:26 +0530

    added navbar and updated other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/HomePage'
-import LoginButton from './components/LoginButton'
+import { Routes, Route } from 'react-router-dom'
+import Favourites from './pages/Favourites'
+import LoginPage from './pages/LoginPage'
+import NavBar from './components/NavBar'
 
 function App() {
   
 
   return (
-    <>
-      <LoginButton />
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
