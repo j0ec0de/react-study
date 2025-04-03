232d3a855e9e27b226eb6a5b51ade592e2c9978c
Unknown <joelanto386@gmail.com>
2025-04-03 17:29:40 +0530

    updated moviecard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,9 @@ function MovieCard({movie}) {
     }
 
     return <div className="movie-card">
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} height={300} width={200}/>
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} height={200} width={150}/>
         <div className="movie-overlay">
-            <button className="favourite-btn" onClick={onFavouriteClick}>Add Favourite ❤</button>
+            <button className="favourite-btn" onClick={onFavouriteClick}>❤</button>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
